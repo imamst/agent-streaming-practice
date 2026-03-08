45837a265f9a7e0b5be776aa071b7f9638321508
Imam Setiawan <s.imam29@gmail.com>
2026-03-08 17:20:50 +0700

    add user prompt

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from app.prompt import SYSTEM_PROMPT
-from openai.types.responses import ResponseTextDeltaEvent
+from openai.types.responses import ResponseFunctionToolCall, ResponseTextDeltaEvent
 from app.tools import search_web
 from app.llm import llm_model
-from agents import Agent, RawResponsesStreamEvent, Runner, set_tracing_disabled
+from agents import (
+    Agent,
+    RawResponsesStreamEvent,
+    Runner,
+    set_tracing_disabled,
+    RunItemStreamEvent,
+)
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,7 +16,7 @@ load_dotenv()
 set_tracing_disabled(True)
 
 
-async def main():
+async def main(input):
     agent: Agent = Agent(
         name="Assistant",
         instructions=SYSTEM_PROMPT,
@@ -18,7 +24,7 @@ async def main():
         tools=[search_web],
     )
 
-    runner = Runner.run_streamed(agent, input="Hey!")
+    runner = Runner.run_streamed(agent, input=input)
 
     async for event in runner.stream_events():
         if event.type == "raw_response_event" and isinstance(
@@ -26,9 +32,14 @@ async def main():
         ):
             if isinstance(event.data, ResponseTextDeltaEvent):
                 print(event.data.delta, end="", flush=True)
+        elif isinstance(event, RunItemStreamEvent) and event.name == "tool_called":
+            if isinstance(event.item.raw_item, ResponseFunctionToolCall):
+                print(f"[TOOL_CALL]: ${event.item.raw_item.name}")
+                print(f"[TOOL_ARGUMENT]: ${event.item.raw_item.arguments}")
 
 
 if __name__ == "__main__":
     import asyncio
 
-    asyncio.run(main())
+    user_input = input("Enter your prompt: ")
+    asyncio.run(main(user_input))
--- app/prompt.py
+++ app/prompt.py
@@ -2,7 +2,8 @@ SYSTEM_PROMPT = """
 You are a helpful assistant
 
 <guidelines>
-- Be more verbose
+- Before you answer, always tell the user, the plan you will do
+- And inform the user what you will do before calling the tools
 - Make a light joke each time you answer
 </guidelines>
 """
