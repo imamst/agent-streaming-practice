4f4111cb0829c4f842a76472a690d0c284959231
Imam Setiawan <s.imam29@gmail.com>
2026-03-08 15:07:16 +0700

    create agent and runner

---
+++ app/llm.py
@@ -0,0 +1,8 @@
+import os
+from agents.extensions.models.litellm_model import LitellmModel
+
+llm_model = LitellmModel(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv("OPENROUTER_API_KEY"),
+    model="openrouter/openai/gpt-5.2"
+)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from agent-streaming!")
+from app.llm import llm_model
+from agents import Agent, Runner, set_tracing_disabled
+from dotenv import load_dotenv
 
+load_dotenv()
+
+set_tracing_disabled(True)
+
+def main():
+    agent: Agent = Agent(name="Assistant", instructions="You are a helpful assistant", model=llm_model)
+
+    runner = Runner.run_sync(agent, input="Hey!")
+
+    print(runner.final_output)
 
 if __name__ == "__main__":
     main()
