caf2752f354ea562c7e35d3b76739f6fc5813192
Imam Setiawan <s.imam29@gmail.com>
2026-03-08 22:15:14 +0700

    add label

--- app/tools.py
+++ app/tools.py
@@ -9,7 +9,13 @@ tavily_client = TavilyClient(os.getenv("TAVILY_API_KEY"))
 
 
 @function_tool
-def search_web(query: str):
+def search_web(query: str, label: str | None = None):
+    """
+    Search the web for information using Tavily API.
+
+    query: The search query to use
+    label: The label to use to tell what you are currently doing with this tool
+    """
     results = tavily_client.search(query, max_results=1)
 
     return results
